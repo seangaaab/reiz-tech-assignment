d88193d6fdef37e60d535b16822de1a8d9bd1d19
seangaaab <seangabbayron@gmail.com>
2023-03-13 12:24:00 +0800

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,15 @@ type Country = {
 };
 
 const App: React.FC = () => {
-  const [countries, setCountries] = useState<Country[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const countriesPerPage: number = 20;
-  const [originalList, setOriginalList] = useState<Country[]>([]);
-  const [toggleSort, setToggleSort] = useState<boolean>(true);
-  const [filters, setFilters] = useState<string[]>([]);
 
+  const [loading, setLoading] = useState<boolean>(false);
+  const [countries, setCountries] = useState<Country[]>([]); // displayed countries based on active filters
+  const [originalList, setOriginalList] = useState<Country[]>([]); // stores copy of original list
+  const [toggleSort, setToggleSort] = useState<boolean>(true); // for alphabetical sort
+  const [filters, setFilters] = useState<string[]>([]); // array of active filters
+  const [checked, setChecked] = useState<string[]>([]); // array of checked checkboxes
+
+  // useEffect that fetches the data, formats and adds it to another array, and initially sorts it alphabetically
   useEffect(() => {
     const fetchCountries = async () => {
       setLoading(true);
@@ -25,20 +26,26 @@ const App: React.FC = () => {
         "https://restcountries.com/v2/all?fields=name,region,area"
       );
       const data = await response.json();
-      const formattedData = data.map((country: any) => ({
-        name: country.name,
-        region: country.region,
-        area: country.area
-      }));
+      const formattedData = data.map((country: any) => (
+        {
+          name: country.name,
+          region: country.region,
+          area: country.area
+        }
+      ));
       setCountries(formattedData);
-      setLoading(false);
+      setOriginalList(formattedData);
+      setLoading(false); // set loading to false if api was fetched
     };
+
     fetchCountries();
-    sortByName("asc")
+    sortByName("asc");
     setOriginalList(countries);
   }, []);
 
-  useEffect(()=>{
+
+  // useEffect that filters the data based on the filters array
+  useEffect(() => {
     let filteredCountries: Country[] = originalList;
 
     filters.forEach((filter) => {
@@ -53,22 +60,25 @@ const App: React.FC = () => {
           break;
       }
     });
-
     setCountries(filteredCountries);
-    setCurrentPage(1);
-    console.log(filters)
-  },[filters])
+    setToggleSort(true); //sorts the list to asc
+    setCurrentPage(1); // returns to page 1 if filter is toggled
+  }, [filters])
 
+  // Pagination
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const countriesPerPage: number = 15;
   const indexOfLastCountry: number = currentPage * countriesPerPage;
   const indexOfFirstCountry: number = indexOfLastCountry - countriesPerPage;
   let currentCountries: Country[] = []
-  currentCountries = countries.slice(
+  currentCountries = countries.slice( // gets the countries based on page number
     indexOfFirstCountry,
     indexOfLastCountry
   );
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
+  // Sort Function
   const sortByName = (arrangement: string) => {
     let sortedCountries: Country[] = [];
 
@@ -82,9 +92,10 @@ const App: React.FC = () => {
       );
     }
     setCountries(sortedCountries);
-    setCurrentPage(1);
+    setCurrentPage(1); // returns to page 1 if sort is toggled
   };
 
+  // Sort Button Handler
   const sortBtn = () => {
     setToggleSort(!toggleSort);
     if (toggleSort) {
@@ -94,23 +105,25 @@ const App: React.FC = () => {
     }
   }
 
-  const [checked, setChecked] = useState<string[]>([]);
-
-  const handleCheckBoxToggle = (value:string) => {
+  // Filter Handlers
+  const handleCheckBoxToggle = (value: string) => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
 
-    if(currentIndex === -1){
+    // if filter is not in the filter array or ===-1 , it adds the filter
+    if (currentIndex === -1) {
       newChecked.push(value);
     }
-    else{
+    else { // else, removes the filter from array
       newChecked.splice(currentIndex, 1);
     }
 
+    // update array of active filters and checked checkboxes
     setChecked(newChecked);
     setFilters(newChecked);
   }
 
+  // Reset Filters, reverts to original list
   const resetFilters = () => {
     setCountries(originalList);
     setToggleSort(true);
@@ -120,39 +133,56 @@ const App: React.FC = () => {
   }
 
   return (
-    <div>
+    <div id="App">
       {loading ? (
-        <p>Loading...</p>
+        <section id="loadingSection">
+          <h2>Fetching Data</h2>
+          <h4>Please wait...</h4>
+        </section>
       ) : (
-        <div>
-          <div>
+        <section id="mainSection">
+
+          <h1>Country List</h1>
+          <header>
             <button onClick={() => sortBtn()}>
               {
                 toggleSort ? "Sort A-Z↥" : "Sort Z-A↧"
               }
             </button>
 
-            {
-              filterList.map((filter) => {
+            <>
+              {
+                filterList.map((filter) => {
+                  return (
+                    <span key={filter.id}>
+                      <input
+                        type="checkbox"
+                        id={filter.name}
+                        checked={checked.indexOf(filter.name) === -1 ? false : true}
+                        onChange={() => {
+                          handleCheckBoxToggle(filter.name)
+                        }}
+                      />
+                      <label htmlFor={filter.name}>{filter.displayName}</label>
+                    </span>
+                  )
+                })
+              }
+            </>
 
-                return (
-                  <span key={filter.id}>
-                    <input
-                      type="checkbox"
-                      id={filter.name}
-                      checked={checked.indexOf(filter.name) === -1 ? false : true}
-                      onChange={() => {
-                        handleCheckBoxToggle(filter.name)
-                      }}
-                    />
-                    <label htmlFor={filter.name}>{filter.displayName}</label>
-                  </span>
-                )
-              })
-            }
+            <button onClick={() => resetFilters()}>Reset Filters</button>
+          </header>
 
-            <button onClick={() => resetFilters()}> Reset</button>
-          </div>
+          <span className="currentPage">
+            Page {currentPage} out of {Math.ceil(countries.length / countriesPerPage)}
+          </span>
+
+          <Pagination
+            countriesPerPage={countriesPerPage}
+            totalCountries={countries.length}
+            currentPage={currentPage}
+            paginate={paginate}
+          />
 
           <table>
             <thead>
@@ -172,12 +202,19 @@ const App: React.FC = () => {
               ))}
             </tbody>
           </table>
+
           <Pagination
             countriesPerPage={countriesPerPage}
             totalCountries={countries.length}
+            currentPage={currentPage}
             paginate={paginate}
           />
-        </div>
+
+          <footer>
+            <p>Developed by: <a href="https://www.linkedin.com/in/seangaaab/">Sean Gabriel C. Bayron</a></p>
+          </footer>
+        </section>
+
       )}
     </div>
   );
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,32 +1,63 @@
+import styles from "./Pagination.module.css"
+
 interface PaginationProps {
     countriesPerPage: number;
     totalCountries: number;
+    currentPage: number;
     paginate: (pageNumber: number) => void;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ countriesPerPage, totalCountries, paginate }) => {
+const back: string = "<";
+const forward: string = ">";
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, countriesPerPage, totalCountries, paginate }) => {
     const pageNumbers = [];
 
+    // computes for total number of pages and adds it to array
     for (let i = 1; i <= Math.ceil(totalCountries / countriesPerPage); i++) {
         pageNumbers.push(i);
     }
 
     return (
-        <nav>
-            <ul className="pagination">
-                {pageNumbers.map((number: number) => (
-                    <li key={number} className="page-item">
-                        <a
-                            className="page-link"
+        <nav id={styles.pagination}>
+            <ul className={styles.listContainer}>
+                {   // hides back button if 1st page
+                    currentPage === 1 ? (
+                        <li className={styles.disabled}>{back}</li>
+                    ) : (
+                        <li
+                            onClick={(e) => {
+                                e.preventDefault();
+                                paginate(currentPage - 1);
+                            }}
+                        >{back}</li>
+                    )
+                }
+                {
+                    pageNumbers.map((number: number) => (
+                        <li
+                            key={number}
+                            className={currentPage == number ? styles.active : ""}
                             onClick={(e) => {
                                 e.preventDefault();
                                 paginate(number);
                             }}
                         >
                             {number}
-                        </a>
-                    </li>
-                ))}
+                        </li>
+                    ))}
+                { // hides next button if last page
+                    currentPage === pageNumbers.length ? (
+                        <li className={styles.disabled}>{forward}</li>
+                    ) : (
+                        <li
+                            onClick={(e) => {
+                                e.preventDefault();
+                                paginate(currentPage + 1);
+                            }}
+                        >{forward}</li>
+                    )
+                }
             </ul>
         </nav>
     );
